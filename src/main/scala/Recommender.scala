993e58528480821eedf55d814e82f2c21840f04f
Oliver Wickham <mr.o.wickham@gmail.com>
2014-11-13 19:02:52 +0000

    Implementation of recommender which loads from wikipedia links file

--- src/main/scala/Recommender.scala
+++ src/main/scala/Recommender.scala
@@ -1,9 +1,10 @@
 package org.my
 
+import java.io.File
+
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
-import org.apache.spark.mllib.linalg.SparseVector
 
 
 object Recommender extends App {
@@ -13,31 +14,38 @@ object Recommender extends App {
                         setSparkHome("~/Documents/Spark/spark-1.1.0/").
                         setMaster(s"local[$parallelism]")
 
-  val sc = new SparkContext(sparkConf)
-  val rows = sc.parallelize(Seq((-239L, "590 22 9059"), (-240L, "22 100"), (-241L, "22 100 590")), 2)
+  val outputDir = new File("data/output")
+  deleteRecursively(outputDir)
 
-  val userVectors: RDD[(Long, Array[(Int, Double)])] = {
-    rows.map { case (user, prefsLine) =>
-      val prefs = prefsLine.split(" ").map(_.toInt).map(item => item -> 1.0)
-      (user, prefs)
-    }
+  val sc = new SparkContext(sparkConf)
+
+  val startTime = System.nanoTime()
+  val rows = sc.textFile("data/links-simple-sorted-1000.txt", 2)
+  val linePattern = """(\d+)""".r
+  val recommendationsPerUser = 5
+
+  val userVectors: RDD[(Long, Seq[(Int, Double)])] = rows.map { r =>
+    val numbers = linePattern.findAllIn(r).toIterable
+    val user = numbers.head.toLong
+    val items = numbers.tail.map(i => i.toInt -> 1.0).toArray
+    user -> items
   }
 
   val allCooccurrences = userVectors.flatMap { case (_, prefs) =>
     // Output combinations both ways, e.g. Array(1, 2) => Array((1, 2), (2, 1))
     val prefItems = prefs.map(_._1)
-    (prefItems.combinations(2) ++ prefItems.reverse.combinations(2)).map { x => x(0) -> (x(1) -> 1L) }
+    (prefItems.combinations(2) ++ prefItems.reverse.combinations(2)).map { x => x(0) -> (x(1) -> 1L)}
   }
-
-  val cooccurrencesWithFrequency = allCooccurrences.groupByKey().map { case (key, values) => key -> countItemFrequency(values) }
 
   def countItemFrequency(pairs: Iterable[(Int, Long)]): Iterable[(Int, Long)] = {
-    pairs.groupBy { case (k, _) => k }
-      .map { case (k, v) => k -> v.map(_._2).sum }
+    pairs.groupBy { case (k, _) => k}
+      .map { case (k, v) => k -> v.map(_._2).sum}
   }
 
+  val cooccurrencesWithFrequency = allCooccurrences.groupByKey().map { case (key, values) => key -> countItemFrequency(values)}.cache()
+
   val userAndCooccurrenceCols = userVectors.map { case (user, prefs) =>
-    val relevantCoocurrenceCols = cooccurrencesWithFrequency.filter { case (k, _) => prefs.indices.contains(k) }
+    val relevantCoocurrenceCols = cooccurrencesWithFrequency.filter { case (k, _) => prefs.indices.contains(k)}
     user -> (prefs -> relevantCoocurrenceCols)
   }
 
@@ -48,13 +56,30 @@ object Recommender extends App {
       val score = cooccurrenceCols.foldLeft(0.0) { case (acc, (cooc, freq)) => acc + (prefMap(cooc) * freq)}
       item -> score
     }
-    val recommendations = itemRatings.filter { case (item, rating) => prefMap(item) == 0.0 && rating > 0.0 }.collect
-    user -> recommendations.sortBy(_._2)(Ordering[Double].reverse)
+    val recommendations = itemRatings
+      .filter { case (item, rating) => prefMap(item) == 0.0 && rating > 0.0}.sortBy({ case (k, v) => v}, true)
+      .take(recommendationsPerUser)
+    user -> recommendations
   }
 
-  println(userVectors.collect.map { case (k, v) => println(k + ":" + v.toString)})
-  cooccurrencesWithFrequency.foreach(println)
-  println("Cols")
-  userPreferences.foreach { case (user, prefs) => println(user + "/" + prefs.mkString(","))}
+  userPreferences.saveAsTextFile(outputDir.getCanonicalPath)
+
+  val durationSecs = (System.nanoTime() - startTime) / (1000 * 1000 * 1000)
+  println(s"Completed in $durationSecs seconds")
+
+  Console.readLine()
+
   sc.stop()
+
+  def deleteRecursively(file: File): Unit = {
+    if (file.isDirectory) file.listFiles().map(deleteRecursively)
+    file.delete()
+  }
+
+  def timeSecs(block: => Any): Long = {
+    val t0 = System.nanoTime()
+    val result = block
+    val t1 = System.nanoTime()
+    (t1 - t0) / (1000 * 1000)
+  }
 }
