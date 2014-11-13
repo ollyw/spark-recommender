2fedd66a8f7d6d482b9f294ced6571c556eb4824
Oliver Wickham <mr.o.wickham@gmail.com>
2014-11-13 16:12:10 +0000

    First pass of basic recommender

--- src/main/scala/Recommender.scala
+++ src/main/scala/Recommender.scala
@@ -1,5 +1,6 @@
 package org.my
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.linalg.SparseVector
@@ -13,28 +14,47 @@ object Recommender extends App {
                         setMaster(s"local[$parallelism]")
 
   val sc = new SparkContext(sparkConf)
-  val rows = sc.parallelize(Seq((-239L, "590 22 9059"), (-240L, "22 100"), (-241L, "22 100 590")))
+  val rows = sc.parallelize(Seq((-239L, "590 22 9059"), (-240L, "22 100"), (-241L, "22 100 590")), 2)
 
-  val userVectors = rows.map { case (user, prefsLine) =>
-    val prefs = prefsLine.split(" ").map(_.toInt)
-    val scores = Array.fill(prefs.size)(1.0)
-    (user, new SparseVector(prefs.size, prefs, scores))
+  val userVectors: RDD[(Long, Array[(Int, Double)])] = {
+    rows.map { case (user, prefsLine) =>
+      val prefs = prefsLine.split(" ").map(_.toInt).map(item => item -> 1.0)
+      (user, prefs)
+    }
   }
 
-  val allCooccurences = userVectors.flatMap { case (_, vector) =>
+  val allCooccurrences = userVectors.flatMap { case (_, prefs) =>
     // Output combinations both ways, e.g. Array(1, 2) => Array((1, 2), (2, 1))
-    (vector.indices.combinations(2) ++ vector.indices.reverse.combinations(2)).map { x => x(0) -> (x(1) -> 1L) }
+    val prefItems = prefs.map(_._1)
+    (prefItems.combinations(2) ++ prefItems.reverse.combinations(2)).map { x => x(0) -> (x(1) -> 1L) }
   }
 
-  val cooccurencesWithFrequency = allCooccurences.groupByKey().map { case (key, values) => key -> countItemFrequency(values) }
+  val cooccurrencesWithFrequency = allCooccurrences.groupByKey().map { case (key, values) => key -> countItemFrequency(values) }
 
   def countItemFrequency(pairs: Iterable[(Int, Long)]): Iterable[(Int, Long)] = {
     pairs.groupBy { case (k, _) => k }
       .map { case (k, v) => k -> v.map(_._2).sum }
   }
 
+  val userAndCooccurrenceCols = userVectors.map { case (user, prefs) =>
+    val relevantCoocurrenceCols = cooccurrencesWithFrequency.filter { case (k, _) => prefs.indices.contains(k) }
+    user -> (prefs -> relevantCoocurrenceCols)
+  }
+
+  // For all co-occurrences, times the user's preference with the number of co-occurrences, sum the total and store against the item
+  val userPreferences: RDD[(Long, Iterable[(Int, Double)])] = userVectors.map { case (user, prefs) =>
+    val prefMap: Map[Int, Double] = prefs.toMap.withDefaultValue(0.0)
+    val itemRatings = cooccurrencesWithFrequency.map { case (item, cooccurrenceCols) =>
+      val score = cooccurrenceCols.foldLeft(0.0) { case (acc, (cooc, freq)) => acc + (prefMap(cooc) * freq)}
+      item -> score
+    }
+    val recommendations = itemRatings.filter { case (item, rating) => prefMap(item) == 0.0 && rating > 0.0 }.collect
+    user -> recommendations.sortBy(_._2)(Ordering[Double].reverse)
+  }
+
   println(userVectors.collect.map { case (k, v) => println(k + ":" + v.toString)})
-  //println(cooccurencesWithFrequency.collect.length)
-  cooccurencesWithFrequency.foreach(println)
+  cooccurrencesWithFrequency.foreach(println)
+  println("Cols")
+  userPreferences.foreach { case (user, prefs) => println(user + "/" + prefs.mkString(","))}
   sc.stop()
 }
